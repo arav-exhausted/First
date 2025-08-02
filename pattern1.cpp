791969a565397959a27e1f290004dac6907172f0
arav-exhausted <aravprajapat1102@gmail.com>
2025-08-02 15:06:24 +0530

    Pattern1
    pattern on while loop practice 1

---
+++ pattern1.cpp
@@ -0,0 +1,60 @@
+
+/*
+
+Pattern for stars-1
+
+like if the user gives input to print 5 stars in 5 rows and 5 columns
+
+the output will be 
+
+*****
+*****
+*****
+*****
+*****
+
+
+*/
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main(){
+    
+
+
+    int n;
+    cin>>n;
+
+    int i= 1;
+
+    while(i<=n){
+        int j = 1 ;
+
+        while(j<=n){
+             cout<<"*";
+             j = j + 1;
+
+        }
+        cout<<endl;
+       
+
+
+        i = i + 1;
+    }
+
+    
+
+}
